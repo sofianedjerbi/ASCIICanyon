3aa8399c8da554c4a10d34918b245a5672938a63
Kugge <sofiane.djerbi38@gmail.com>
2021-10-10 12:28:39 +0200

    Better defaults values

--- src/main.h
+++ src/main.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #ifndef MAX_SPEED
-#define MAX_SPEED 80   // Max allowed speed
+#define MAX_SPEED 70   // Max allowed speed
 #endif
 
 #ifndef MIN_WALLSIZE
-#define MIN_WALLSIZE 5 // Minimum wall size
+#define MIN_WALLSIZE 8 // Minimum wall size
 #endif
